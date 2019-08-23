e192bda9cb29902bc72702e638f254715584b127
leishiguang <leishiguang@outlook.com>
2019-08-24 00:32:40 +0800

    完成 spring-framework-mini

--- README.md
+++ README.md
@@ -1,3 +1,87 @@
-## 手写简化版 spring framework
+## 300 行代码提炼 spring 核心原理
 
-完善中……
+更深入了解 spring 原理：**[spring-framework-simplified](https://github.com/leishiguang/spring-framework-simplified)**
+
+<p>
+	<a target="_blank" href="https://github.com/leishiguang/spring-framework-mini/blob/master/LICENSE">
+		<img src="https://img.shields.io/apm/l/vim-mode.svg?color=yellow" ></img>
+	</a>
+	<a target="_blank" href="https://www.oracle.com/technetwork/java/javase/downloads/index.html">
+		<img src="https://img.shields.io/badge/JDK-1.8+-green.svg" ></img>
+	</a>
+</p>
+
+---
+
+### 构建方式
+
+1. 导入 build.gradle 工程；
+2. 添加 web 包至 servlet 容器（如 tomcat）；
+3. 启动 servlet 容器；
+
+### 运行效果
+
+在控制台看到末尾出现日志 `Mini Spring Framework is init.` 即表示启动成功。
+此时在浏览器输入：localhost:8080/hello?name=world
+即可看到浏览器返回：hello world
+
+如图所示：
+
+![image](https://raw.githubusercontent.com/leishiguang/spring-framework-mini/master/docs/images/helloWorld.png)
+
+### 原理解析
+
+重点在于 DispatcherServlet 的初始化过程，分为如下几个步骤：
+1. 加载配置文件：获取扫描包的路径；
+2. 扫描相关类：将添加了 @Service 或 @Controller 注解的类名，保存在 List 容器中；
+3. 初始化扫描到的类：以 beanName 作为键值，在 HashMap 中保存初始化好的类对象，即放入 IoC 容器中；
+4. 完成依赖注入：对增加了 @Autowired 注解的属性，注入 IoC 容器中的 bean；
+5. 初始化 HandlerMapping：将 @Controller 类中，增加了 @RequestMapping 注解的方法，依据 url 配置，加入到 HandlerMapping 中；
+
+初始化完毕，请求进来时：
+1. 首先到达 doGet 或者 doPost 方法，并交由 doDispatch 方法进行处理；
+2. 依据 url 从 HandlerMapping 中获取对应的 Handler；
+3. 从 Handler 中取得对应方法的参数列表；
+4. 按 Handler 的参数顺序，从 req 中获取到参数值；
+5. 反射执行 Handler 中保存的方法，获得返回值；
+6. resp 写出返回值，完成请求；
+
+当然，真正的 Spring 要复杂许多，这儿主要是了解 Spring 的基本设计思路，以及设计模式的应用。
+
+更深入了解 spring 原理：**[spring-framework-simplified](https://github.com/leishiguang/spring-framework-simplified)**
+
+### 目录结构
+
+```
+src/main
+├─java
+│  └─simplified
+│      └─spring
+│          ├─annotation 注解支持
+│          │      Autowired.java 
+│          │      Controller.java
+│          │      RequestMapping.java
+│          │      RequestParam.java
+│          │      Service.java
+│          │
+│          ├─controller 
+│          │      DemoController.java 演示用的Controller
+│          │
+│          ├─service
+│          │  │  DemoService.java 演示用的Service
+│          │  │
+│          │  └─impl
+│          │          DemoServiceImpl.java 演示用的Service实现
+│          │
+│          └─servlet
+│                  DispatcherServlet.java Servlet入口
+│
+├─resources
+│      application.properties
+│
+└─webapp
+    └─WEB-INF
+            web.xml
+```
+
+最后祝大家生活愉快~
--- src/main/java/simplified/spring/servlet/DispatcherServlet.java
+++ src/main/java/simplified/spring/servlet/DispatcherServlet.java
@@ -1,9 +1,6 @@
 package simplified.spring.servlet;
 
-import simplified.spring.annotation.Autowired;
-import simplified.spring.annotation.Controller;
-import simplified.spring.annotation.RequestMapping;
-import simplified.spring.annotation.Service;
+import simplified.spring.annotation.*;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServlet;
@@ -12,14 +9,17 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
- * 初始化的入口
+ * Servlet 初始化入口
  *
  * @author leishiguang
  * @since v1.0
@@ -42,12 +42,17 @@ public class DispatcherServlet extends HttpServlet {
 	private Map<String, Object> ioc = new HashMap<>();
 
 	/**
-	 * 保存 url 和 Method 的对应关系
+	 * 保存 url -> Method 的对应关系
 	 */
-	private Map<String, Object> handlerMapping = new HashMap<>();
+	private List<Handler> handlerMapping = new ArrayList<>();
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		this.doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		try {
 			doDispatch(req, resp);
 		} catch (Exception e) {
@@ -57,38 +62,54 @@ public class DispatcherServlet extends HttpServlet {
 	}
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		this.doGet(req, resp);
-	}
-
-	@Override
-	public void init(ServletConfig config){
-		// 加载配置文件
+	public void init(ServletConfig config) {
+		//加载配置文件
 		doLoadConfig(config.getInitParameter("contextConfigLocation"));
-		// 扫描相关类
+		//扫描相关类
 		doScanner(contextConfig.getProperty("scanPackage"));
-		// 初始化扫描到的类，把他们放入 Ioc 容器中
+		//初始化扫描到的类，把他们放入 Ioc 容器中
 		doInstance();
-		// 完成依赖注入
+		//完成依赖注入
 		doAutowired();
-		// 初始化 HandlerMapping
+		//初始化 HandlerMapping
 		initHandlerMapping();
 		System.out.println("Mini Spring Framework is init.");
 	}
 
 
 	private void doDispatch(HttpServletRequest req, HttpServletResponse resp) throws IOException, InvocationTargetException, IllegalAccessException {
-		String url = req.getRequestURI();
-		String contextPath = req.getContextPath();
-		url = url.replace(contextPath, "").replace("/+", "/");
-		if (!this.handlerMapping.containsKey(url)) {
+		Handler handler = getHandler(req);
+		if (handler == null) {
 			resp.getWriter().write("404 Not Found！");
+			return;
 		}
-		Method method = (Method) this.handlerMapping.get(url);
+		//获得方法的行参列表
+		Class<?>[] paramTypes = handler.getParamTypes();
+		Object[] paramValues = new Object[paramTypes.length];
 		Map<String, String[]> params = req.getParameterMap();
-		String beanName = toLowerFirseCase(method.getDeclaringClass().getSimpleName());
-		Object object = ioc.get(beanName);
-		method.invoke(object, req, resp, params.get("name")[0]);
+		for (Map.Entry<String, String[]> param : params.entrySet()) {
+			String value = Arrays.toString(param.getValue())
+					.replaceAll("[\\[\\]]", "")
+					.replaceAll("\\s", ",");
+			if (!handler.paramIndexMapping.containsKey(param.getKey())) {
+				continue;
+			}
+			int index = handler.paramIndexMapping.get(param.getKey());
+			paramValues[index] = convert(paramTypes[index], value);
+		}
+		if (handler.paramIndexMapping.containsKey(HttpServletRequest.class.getName())) {
+			int reqIndex = handler.paramIndexMapping.get(HttpServletRequest.class.getName());
+			paramValues[reqIndex] = req;
+		}
+		if (handler.paramIndexMapping.containsKey(HttpServletResponse.class.getName())) {
+			int reqIndex = handler.paramIndexMapping.get(HttpServletResponse.class.getName());
+			paramValues[reqIndex] = resp;
+		}
+		Object returnValue = handler.method.invoke(handler.controller,paramValues);
+		if(returnValue == null || returnValue instanceof Void){
+			return;
+		}
+		resp.getWriter().write(returnValue.toString());
 	}
 
 
@@ -98,8 +119,8 @@ public class DispatcherServlet extends HttpServlet {
 	 * @param contextConfigLocation contextConfigLocation
 	 */
 	private void doLoadConfig(String contextConfigLocation) {
-		// 直接通过类路径，找到 Spring 主配置文件所在路径
-		// 将其读取出来，并放到 Properties 中
+		//直接通过类路径，找到 Spring 主配置文件所在路径
+		//将其读取出来，并放到 Properties 中
 		try (InputStream fis = this.getClass().getClassLoader().getResourceAsStream(contextConfigLocation)) {
 			if (fis == null) {
 				throw new NullPointerException("读取配置不能为空");
@@ -153,15 +174,15 @@ public class DispatcherServlet extends HttpServlet {
 				} catch (InstantiationException | IllegalAccessException e) {
 					e.printStackTrace();
 				}
-				// bean的命名格式以小写开头
-				String beanName = toLowerFirseCase(clazz.getSimpleName());
+				//bean的命名格式以小写开头
+				String beanName = toLowerFirstCase(clazz.getSimpleName());
 				ioc.put(beanName, instance);
 			} else if (clazz.isAnnotationPresent(Service.class)) {
-				// 自定义bean名称
+				//自定义bean名称
 				Service service = clazz.getAnnotation(Service.class);
 				String beanName = service.value();
 				if ("".equals(beanName)) {
-					beanName = toLowerFirseCase(clazz.getSimpleName());
+					beanName = toLowerFirstCase(clazz.getSimpleName());
 				}
 				Object instance = null;
 				try {
@@ -170,12 +191,12 @@ public class DispatcherServlet extends HttpServlet {
 					e.printStackTrace();
 				}
 				ioc.put(beanName, instance);
-				// 根据类型自动赋值
+				//根据类型自动赋值
 				for (Class<?> i : clazz.getInterfaces()) {
 					if (ioc.containsKey(i.getName())) {
 						throw new RuntimeException("The '" + i.getName() + "' is exists!");
 					}
-					// 把接口的类型直接当成key
+					//把接口的类型直接当成key
 					ioc.put(i.getName(), instance);
 				}
 			}
@@ -186,9 +207,9 @@ public class DispatcherServlet extends HttpServlet {
 	 * 首字母小写
 	 *
 	 * @param simpleName 首字母要变成小写的字符
-	 * @return 首字母小写
+	 * @return 首字母转为小写
 	 */
-	private String toLowerFirseCase(String simpleName) {
+	private String toLowerFirstCase(String simpleName) {
 		char[] chars = simpleName.toCharArray();
 		chars[0] = Character.toLowerCase(chars[0]);
 		return String.valueOf(chars);
@@ -200,8 +221,8 @@ public class DispatcherServlet extends HttpServlet {
 	private void doAutowired() {
 		assert ioc.size() != 0;
 		for (Map.Entry<String, Object> entry : ioc.entrySet()) {
-			// 获取所有的字段，包括 private、protected、public 类型的
-			// 正常来讲，普通的 OOP 编程只能获得 public 类型的字段
+			//获取所有的字段，包括 private、protected、public 类型的
+			//正常来讲，普通的 OOP 编程只能获得 public 类型的字段
 			Field[] fields = entry.getValue().getClass().getDeclaredFields();
 			for (Field field : fields) {
 				if (!field.isAnnotationPresent(Autowired.class)) {
@@ -211,8 +232,8 @@ public class DispatcherServlet extends HttpServlet {
 				//如果用户没有自定义 beanName，默认就根据类型注入
 				String beanNeme = autowired.value();
 				if ("".equals(beanNeme)) {
-					// 这儿的 beanName 是接口类的名字
-					beanNeme = toLowerFirseCase(field.getType().getName());
+					//这儿的 beanName 是接口类的名字
+					beanNeme = toLowerFirstCase(field.getType().getName());
 				}
 				//如果是public以外的类型，只要加了 @Autowired 注解都要强制赋值
 				field.setAccessible(true);
@@ -235,22 +256,121 @@ public class DispatcherServlet extends HttpServlet {
 			if (!clazz.isAnnotationPresent(Controller.class)) {
 				continue;
 			}
-			//保存在类上面的RequestMapping
+			//保存在类上面的 RequestMapping 和类上面的 url 配置
 			String baseUrl = "";
 			if (clazz.isAnnotationPresent(RequestMapping.class)) {
 				RequestMapping requestMapping = clazz.getAnnotation(RequestMapping.class);
 				baseUrl = requestMapping.value();
 			}
-			// 默认获取所有的 public 类型的方法
+			//默认获取所有的 public 类型的方法，以及 Method 的 url 配置
 			for (Method method : clazz.getMethods()) {
 				if (!method.isAnnotationPresent(RequestMapping.class)) {
 					continue;
 				}
+				//映射 url
 				RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
 				String url = ("/" + baseUrl + "/" + requestMapping.value()).replaceAll("/+", "/");
-				handlerMapping.put(url, method);
-				System.out.println("Mapped : " + url + "," + method);
+				Pattern pattern = Pattern.compile(url);
+				handlerMapping.add(new Handler(entry.getValue(), method, pattern));
+				System.out.println("Mapped : " + url + " -> " + method);
 			}
 		}
 	}
+
+	/**
+	 * 获取对应 Handler
+	 */
+	private Handler getHandler(HttpServletRequest req){
+		assert handlerMapping.size() > 0;
+		String url = req.getRequestURI();
+		String contextPath = req.getContextPath();
+		url = url.replace(contextPath,"").replaceAll("/+","/");
+		for(Handler handler:handlerMapping){
+			Matcher matcher = handler.pattern.matcher(url);
+			if(matcher.matches()){
+				return handler;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * 由于 HTTP 基于字符串协议，url 传过来的参数都是 String 类型的
+	 * 只需要把 String 转换为任意类型
+	 */
+	private Object convert(Class<?> type, String value) {
+		if (Integer.class == type) {
+			return Integer.valueOf(value);
+		}
+		// 如果还有 double 或者其它类型的参数，继续增加 if
+		// 可以使用策略模式进行优化，这儿略。
+		return value;
+	}
+
+	/**
+	 * 内部类，Handler 记录 Controller 中 RequestMapping 和 Method 的对应关系
+	 */
+	private static class Handler {
+		/**
+		 * 保存方法对应的实例
+		 */
+		private Object controller;
+
+		/**
+		 * 保存映射的方法
+		 */
+		private Method method;
+
+		/**
+		 * 正则表达式
+		 */
+		private Pattern pattern;
+
+
+		/**
+		 * 参数顺序
+		 */
+		private Map<String, Integer> paramIndexMapping;
+
+		Handler(Object controller, Method method, Pattern pattern) {
+			this.controller = controller;
+			this.method = method;
+			this.pattern = pattern;
+			paramIndexMapping = new HashMap<>(6);
+			putParamIndexMapping(method);
+		}
+
+		/**
+		 * 提取方法中加了注解的参数，以及 Request 和 Response 参数
+		 */
+		private void putParamIndexMapping(Method method) {
+			Annotation[][] pa = method.getParameterAnnotations();
+			for (int i = 0; i < pa.length; i++) {
+				for (Annotation a : pa[i]) {
+					if (a instanceof RequestParam) {
+						String paramName = ((RequestParam) a).value();
+						if (!"".equals(paramName.trim())) {
+							paramIndexMapping.put(paramName, i);
+						}
+					}
+				}
+			}
+			//提取方法中的 request 和 response 参数
+			Class<?>[] paramsTypes = method.getParameterTypes();
+			for (int i = 0; i < paramsTypes.length; i++) {
+				Class<?> type = paramsTypes[i];
+				if (type == HttpServletRequest.class || type == HttpServletResponse.class) {
+					paramIndexMapping.put(type.getName(), i);
+				}
+			}
+		}
+
+		/**
+		 * 获取方法所有需要的参数
+		 */
+		private Class<?>[] getParamTypes() {
+			return method.getParameterTypes();
+		}
+	}
+
 }
