dcb1f21d23106f0be87bd8e13ee389dc8169a4e0
leishiguang <leishiguang@outlook.com>
2019-08-28 16:21:20 +0800

    增加开始/完成 bean 加载的回调

--- src/main/java/simplified/spring/beans/config/BeanPostProcessor.java
+++ src/main/java/simplified/spring/beans/config/BeanPostProcessor.java
@@ -1,24 +1,27 @@
 package simplified.spring.beans.config;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * 为对象初始化事件设置一种回调机制，这儿只做说明
  *
  * @author leishiguang
  * @since v1.0
  */
+@Slf4j
 public class BeanPostProcessor {
 
 	/**
 	 * 为 Bean 的初始化之前提供回调入口
 	 */
-	public Object postProcessBeforeInitialization(Object bean, String beanName){
-		return bean;
+	public void postProcessBeforeInitialization(Object bean, String beanName) {
+		log.info("开始加载bean [" + beanName + "] " + bean);
 	}
 
 	/**
 	 * 为 Bean 的初始化之后提供回调入口
 	 */
-	public Object postProcessAfterInitialization(Object bean, String beanName){
-		return bean;
+	public void postProcessAfterInitialization(Object bean, String beanName) {
+		log.info("完成加载bean [" + beanName + "] " + bean);
 	}
 }
